1e2d6a048abdb06b1fe9430662e48e5af97b2ee5
ruooooooli <ruooooooli@gmail.com>
2016-10-14 09:52:04 +0800

    fix gulpfilejs and add some fonts

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,12 @@ require('laravel-elixir-vue');
 
 elixir(mix => {
     mix
-        // .copy('semantic/dist/semantic.js', 'resources/assets/js/semantic.js')
-        // .copy('semantic/dist/semantic.css', 'resources/assets/css/semantic.css')
-        // .copy('semantic/dist/semantic.min.js', 'resources/assets/js/semantic.min.js')
-        // .copy('semantic/dist/semantic.min.css', 'resources/assets/css/semantic.min.css')
-        // .copy('semantic/src/themes/default/assets/fonts/*', 'public/assets/fonts/')
-        // .copy('semantic/src/themes/default/assets/images/*', 'public/assets/images/')
-
-        // .copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/jquery.min.js')
-
-        // .copy('node_modules/nprogress/nprogress.js', 'resources/assets/js/nprogress.js')
-        // .copy('node_modules/nprogress/nprogress.css', 'resources/assets/css/nprogress.css')
-
-        // .copy('node_modules/sweetalert/dist/sweetalert.css', 'resources/assets/css/sweetalert.css')
-        // .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'resources/assets/js/sweetalert.min.js')
-        // .copy('node_modules/marked/marked.min.js', 'resources/assets/js/marked.min.js')
+        .copy('semantic/dist/semantic.js', 'resources/assets/js/semantic.js')
+        .copy('semantic/dist/semantic.css', 'resources/assets/css/semantic.css')
+        .copy('semantic/dist/semantic.min.js', 'resources/assets/js/semantic.min.js')
+        .copy('semantic/dist/semantic.min.css', 'resources/assets/css/semantic.min.css')
+        .copy('semantic/src/themes/default/assets/fonts', 'public/assets/fonts/')
+        .copy('semantic/src/themes/default/assets/images', 'public/assets/images/')
 
         .sass('markdown.scss', 'resources/assets/css/markdown-one.css')
         .sass('prism.scss', 'resources/assets/css/prism.css')

ed3a9ba23818b79351f9bc27a3a82cceb1b64d67
ruooooooli <ruooooooli@gmail.com>
2016-10-26 18:53:48 +0800

    fix paginate

--- resources/views/layout/backend/table_paginate.blade.php
+++ resources/views/layout/backend/table_paginate.blade.php
@@ -1,7 +1,5 @@
-@if(count($value) > config('blog.pageSize'))
-    <tr>
-        <th colspan="{{ $colspan }}" class="center aligned">
-            {{ $value->links('layout.backend.paginate') }}
-        </th>
-    </tr>
-@endif
+<tr>
+    <th colspan="{{ $colspan }}" class="center aligned">
+        {{ $value->links('layout.backend.paginate') }}
+    </th>
+</tr>

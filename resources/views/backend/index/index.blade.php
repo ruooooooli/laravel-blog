a908b55632ca17021440af5a862e49221c0a09df
ruooooooli <ruooooooli@gmail.com>
2016-11-02 11:37:37 +0800

    fix css body

--- resources/views/backend/index/index.blade.php
+++ resources/views/backend/index/index.blade.php
@@ -3,6 +3,9 @@
 @section('title', '首页')
 
 @section('content')
+<h4 class="ui horizontal divider header">
+    <i class="home icon"></i>项目预览
+</h4>
 <div class="ui equal width grid">
     <div class="row">
         <div class="column">
@@ -83,27 +86,39 @@
         </div>
     </div>
 </div>
-<div class="ui raised segments">
-    <div class="ui segment">
-        <h4>服务器时间 : {{ date('Y-m-d  H:i:s') }}</h4>
-    </div>
-    <div class="ui segment">
-        <h4>服务器 IP : {{ $_SERVER['SERVER_NAME'] . '(' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . ')' }}</h4>
-    </div>
-    <div class="ui segment">
-        <h4>操作系统 : {{ PHP_OS }}</h4>
-    </div>
-    <div class="ui segment">
-        <h4>数据库 : MYSQL</h4>
-    </div>
-    <div class="ui segment">
-        <h4>PHP 版本 : {{ PHP_VERSION }}</h4>
-    </div>
-    <div class="ui segment">
-        <h4>NGINX 版本 : {{ $_SERVER['SERVER_SOFTWARE'] }}</h4>
-    </div>
-    <div class="ui segment">
-        <h4>上传大小 : {{ ini_get('upload_max_filesize') }}</h4>
-    </div>
-</div>
+<h4 class="ui horizontal divider header">
+    <i class="server icon"></i>站点信息
+</h4>
+<table class="ui definition table">
+    <tbody>
+        <tr>
+            <td class="two wide column">服务器时间</td>
+            <td>{{ date('Y-m-d  H:i:s') }}</td>
+        </tr>
+        <tr>
+            <td class="two wide column">服务器 IP 地址</td>
+            <td>{{ $_SERVER['SERVER_NAME'] . '(' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . ')' }}</td>
+        </tr>
+        <tr>
+            <td class="two wide column">操作系统</td>
+            <td>{{ PHP_OS }}</td>
+        </tr>
+        <tr>
+            <td class="two wide column">数据库</td>
+            <td>MYSQL</td>
+        </tr>
+        <tr>
+            <td class="two wide column">PHP 版本</td>
+            <td>{{ PHP_VERSION }}</td>
+        </tr>
+        <tr>
+            <td class="two wide column">NGINX 版本</td>
+            <td>{{ $_SERVER['SERVER_SOFTWARE'] }}</td>
+        </tr>
+        <tr>
+            <td class="two wide column">上传大小</td>
+            <td>{{ ini_get('upload_max_filesize') }}</td>
+        </tr>
+    </tbody>
+</table>
 @endsection

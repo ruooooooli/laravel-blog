d602fc4665b2c6be127b62c5866844c7e1dce663
= <=>
2017-04-08 23:23:31 +0800

    add some table

--- database/migrations/2017_04_05_165258_create_logs_table.php
+++ database/migrations/2017_04_05_165258_create_logs_table.php
@@ -6,17 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 class CreateLogsTable extends Migration
 {
 
-	public function up()
-	{
-		Schema::create('logs', function(Blueprint $table) {
+    public function up()
+    {
+        Schema::create('logs', function(Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id')->index()->default(0);
+            $table->string();
 
             $table->timestamps();
-		});
-	}
+        });
+    }
 
-	public function down()
-	{
-		Schema::drop('logs');
-	}
+    public function down()
+    {
+        Schema::drop('logs');
+    }
 }
